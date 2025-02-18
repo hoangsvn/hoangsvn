85a70ad175748c6586a8e42f13fc141cab915bd0
Nguyễn Xuân Hoàng <nguyenxuanhoangicloudvn@gmail.com>
2025-02-19 00:06:40 +0700

    Update locket-gold.js

--- module/locket-gold.js
+++ module/locket-gold.js
@@ -1,32 +1,30 @@
 const mapping = {
-    '%E8%BD%A6%E7%A5%A8%E7%A5%A8': 'vip+watch_vip',
-    'Locket': 'Gold'
+    '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+    'Locket': ['Gold']
 };
 
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
-    obj = JSON.parse($response.body);
-
-obj.Attention = "Không Chia sẻ";
-
-var sub = {
-    is_sandbox: false,
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"], obj = JSON.parse($response.body);
+obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+var locket02 = {
+    is_sandbox: !1,
     ownership_type: "PURCHASED",
+    billing_issues_detected_at: null,
+    period_type: "normal",
     expires_date: "2099-12-18T01:04:17Z",
-    purchase_date: "2024-07-28T01:04:17Z",
+    grace_period_expires_date: null,
+    unsubscribe_detected_at: null,
+    original_purchase_date: "2024-04-12T01:04:18Z",
+    purchase_date: "2024-04-12T01:04:17Z",
     store: "app_store"
-};
-
-var product = {
-    purchase_date: "2024-07-28T01:04:17Z",
-    product_identifier: "com.ohoang7.premium.yearly",
+}, locket01 = {
+    grace_period_expires_date: null,
+    purchase_date: "2024-04-12T01:04:17Z",
+    product_identifier: "com.locket02.premium.yearly",
     expires_date: "2099-12-18T01:04:17Z"
 };
-
 const match = Object.keys(mapping).find(e => ua.includes(e));
-obj.subscriber.subscriptions[match ? mapping[match] : product.product_identifier] = sub;
-obj.subscriber.entitlements[match ? mapping[match] : "pro"] = product;
-
-$done({ body: JSON.stringify(obj) });
-
-
-
+if (match) {
+    let [e, s] = mapping[match];
+    s ? (locket01.product_identifier = s, obj.subscriber.subscriptions[s] = locket02) : obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02, obj.subscriber.entitlements[e] = locket01
+} else obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02, obj.subscriber.entitlements.pro = locket01;
+$done({body: JSON.stringify(obj)});
