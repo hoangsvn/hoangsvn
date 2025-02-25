93dfdf69870f2e3b809d4dcdc6e97f21feda4edc
Nguyễn Xuân Hoàng <nguyenxuanhoangicloudvn@gmail.com>
2025-02-25 23:27:43 +0700

    up

--- module/Build/src/locket.js
+++ module/Build/src/locket.js
@@ -2,25 +2,25 @@ const mapping = {
     '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
     'Locket': ['Gold']
 };
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"], obj = JSON.parse($response.body);
+const ua = $request.headers['User-Agent'] || $request.headers['user-agent'], obj = JSON.parse($response.body)
 obj.Attention = "Chúc mừng bạn!";
-var locketsub = {
+const locketsub = {
     is_sandbox: !1,
-    ownership_type: "PURCHASED",
+    ownership_type: 'PURCHASED',
     billing_issues_detected_at: null,
-    period_type: "normal",
-    expires_date: "2099-12-18T01:04:17Z",
+    period_type: 'normal',
+    expires_date: '2099-12-18T01:04:17Z',
     grace_period_expires_date: null,
     unsubscribe_detected_at: null,
-    original_purchase_date: "2024-07-28T01:04:18Z",
-    purchase_date: "2024-07-28T01:04:17Z",
-    store: "app_store"
+    original_purchase_date: '2024-07-28T01:04:18Z',
+    purchase_date: '2024-07-28T01:04:17Z',
+    store: 'app_store'
 }, locket_expires = {
     grace_period_expires_date: null,
-    purchase_date: "2024-07-28T01:04:17Z",
-    product_identifier: "com.ohoang7.premium.yearly",
-    expires_date: "2099-12-18T01:04:17Z"
-};
+    purchase_date: '2024-07-28T01:04:17Z',
+    product_identifier: 'com.ohoang7.premium.yearly',
+    expires_date: '2099-12-18T01:04:17Z'
+}
 const match = Object.keys(mapping).find(e => ua.includes(e));
 if (match) {
     let [e, s] = mapping[match];
