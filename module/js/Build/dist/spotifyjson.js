f4375545afece5a04bfcc6acebcac5746423e02e
Nguyễn Xuân Hoàng <nguyenxuanhoangicloudvn@gmail.com>
2025-02-25 12:07:58 +0700

    up

---
+++ module/js/Build/dist/spotifyjson.js
@@ -0,0 +1,3 @@
+// Build Spotify Start: 2/25/2025, 12:06:01 PM
+(()=>{$done({url:$request.url.replace(/platform=iphone/,"platform=ipad")});})();
+// Build Spotify End: 2/25/2025, 12:06:01 PM
--- module/js/Spotify/dist/spotifyjson.js
+++
@@ -1,3 +0,0 @@
-// Build Spotify Start: 2/25/2025, 10:56:25 AM
-(()=>{$done({url:$request.url.replace(/platform=iphone/,"platform=ipad")});})();
-// Build Spotify End: 2/25/2025, 10:56:25 AM
