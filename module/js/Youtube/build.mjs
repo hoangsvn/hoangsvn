456b0cca335e10e614ebada8f094937d8e7f36a1
Nguyễn Xuân Hoàng <nguyenxuanhoangicloudvn@gmail.com>
2025-02-25 11:01:01 +0700

    up

--- module/js/Youtube/build.mjs
+++ module/js/Youtube/build.mjs
@@ -6,7 +6,7 @@ esbuild.buildSync({
   minify: true,
   banner: { js: `// Build Youtube Start: ${new Date().toLocaleString()}` },
   footer: { js: `// Build Youtube End: ${new Date().toLocaleString()}` },
-  // inject: ['./lib/text-polyfill.mjs'],
+  inject: ['./lib/text-polyfill.mjs'],
   sourcemap: false,
   outfile: './dist/youtube.js'
 })
