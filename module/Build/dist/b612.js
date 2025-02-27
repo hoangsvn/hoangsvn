144ce2fab8bf1b5a4986c81a46283113137d2936
Nguyễn Xuân Hoàng <nguyenxuanhoangicloudvn@gmail.com>
2025-02-27 20:38:30 +0700

    up

---
+++ module/Build/dist/b612.js
@@ -0,0 +1,3 @@
+// Build B612 Start: 2/27/2025, 8:28:49 PM
+(()=>{var t={result:{activated:!0,products:[{productId:"10000",startDate:1e11,expireDate:1e12,managed:!1,status:"active"}]}};$done({body:JSON.stringify(t)});})();
+// Build B612 End: 2/27/2025, 8:28:49 PM
