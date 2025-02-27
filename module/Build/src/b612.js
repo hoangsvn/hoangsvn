1ad9d5c8c48c58211674977df996a3952c49f980
Nguyễn Xuân Hoàng <nguyenxuanhoangicloudvn@gmail.com>
2025-02-27 21:35:27 +0700

    up

--- module/Build/src/b612.js
+++ module/Build/src/b612.js
@@ -1,16 +1,17 @@
 
 
+// const obj = {
+//
+//     result: {
+//         activated: true,
+//         products: [{
+//             productId: '10000',
+//             startDate: 100000000000,  // Có thể là timestamp (năm 1973)
+//             expireDate: 1000000000000, // Có thể là timestamp (năm 2001)
+//             managed: false,
+//             status: 'active'
+//         }]
+//     }
+// }
 console.log(JSON.stringify($response.body));
-const obj = {
-    result: {
-        activated: true,
-        products: [{
-            productId: '10000',
-            startDate: 100000000000,  // Có thể là timestamp (năm 1973)
-            expireDate: 1000000000000, // Có thể là timestamp (năm 2001)
-            managed: false,
-            status: 'active'
-        }]
-    }
-}
-$done({body: JSON.stringify(obj)});
+// $done({body: JSON.stringify(obj)});
