7447a992efdb17f99e1e235bb98fb5831978549f
Nguyễn Xuân Hoàng <nguyenxuanhoangicloudvn@gmail.com>
2025-06-06 02:51:36 +0700

    up

--- extra/dist/soundcloud.js
+++ extra/dist/soundcloud.js
@@ -1,3 +1,3 @@
-// Build Soundcloud Start: 2/25/2025, 11:08:34 PM
+// Build Soundcloud Start: 6/6/2025, 2:50:06 AM
 (()=>{var e=JSON.parse($response.body);e.plan={vendor:"apple",id:"high_tier",manageable:!0,plan_upsells:[],plan_id:"go-plus",upsells:[],plan_name:"SoundCloud Go+"};e.features=[{name:"offline_sync",enabled:!0,plans:["mid_tier","high_tier"]},{name:"no_audio_ads",enabled:!0,plans:["mid_tier","high_tier"]},{name:"hq_audio",enabled:!0,plans:["high_tier"]},{name:"system_playlist_in_library",enabled:!0,plans:[]},{name:"ads_krux",enabled:!1,plans:[]},{name:"new_home",enabled:!0,plans:[]},{name:"spotlight",enabled:!1,plans:[]}];$done({body:JSON.stringify(e)});})();
-// Build Soundcloud End: 2/25/2025, 11:08:34 PM
+// Build Soundcloud End: 6/6/2025, 2:50:06 AM
--- extra/dist/spotifyjson.js
+++ extra/dist/spotifyjson.js
@@ -1,3 +1,3 @@
-// Build Spotify Start: 6/5/2025, 2:01:18 PM
+// Build Spotify Start: 6/6/2025, 2:50:37 AM
 (()=>{$done($request.url.replace(/platform=iphone/,"platform=ipad"));})();
-// Build Spotify End: 6/5/2025, 2:01:18 PM
+// Build Spotify End: 6/6/2025, 2:50:37 AM
