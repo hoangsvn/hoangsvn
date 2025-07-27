0a277749806ab7977968b1d3461147c489ab00b9
hoangsvn <nguyenxuanhoangicloudvn@gmail.com>
2025-07-27 21:40:58 +0000

    Update Module by CICD Action

--- extra/dist/spotifyjson.js
+++ extra/dist/spotifyjson.js
@@ -1,3 +1,3 @@
-// Build Spotify Start: 7/21/2025, 2:05:10 AM
+// Build Spotify Start: 7/27/2025, 9:40:58 PM
 (()=>{$done($request.url.replace(/platform=iphone/,"platform=ipad"));})();
-// Build Spotify End: 7/21/2025, 2:05:10 AM
+// Build Spotify End: 7/27/2025, 9:40:58 PM
