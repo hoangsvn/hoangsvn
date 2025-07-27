7628b5c90f1a2fc91b50fd7abcf32e1864b6f5d7
Nguyễn Xuân Hoàng <nguyenxuanhoangicloudvn@gmail.com>
2025-07-28 05:54:25 +0700

    Update soundcloud.js

--- extra/src/soundcloud.js
+++ extra/src/soundcloud.js
@@ -1,60 +1,59 @@
 const obj = JSON.parse($response.body)
 obj.plan = {
-  'vendor': 'apple',
-  'id': 'high_tier',
-  'manageable': true,
-  'plan_upsells': [],
-  'plan_id': 'go-plus',
-  'upsells': [],
-  'plan_name': 'SoundCloud Go+'
+    'vendor': 'apple',
+    'id': 'high_tier',
+    'manageable': true,
+    'plan_upsells': [],
+    'plan_id': 'go-plus',
+    'upsells': [],
+    'plan_name': 'Go Plus'
 }
 obj.features = [
-  {
-    'name': 'offline_sync',
-    'enabled': true,
-    'plans': [
-      'mid_tier',
-      'high_tier'
-    ]
-  },
-  {
-    'name': 'no_audio_ads',
-    'enabled': true,
-    'plans': [
-      'mid_tier',
-      'high_tier'
-    ]
-  },
-  {
-    'name': 'hq_audio',
-    'enabled': true,
-    'plans': [
-      'high_tier'
-    ]
-  },
-  {
-    'name': 'system_playlist_in_library',
-    'enabled': true,
-    'plans': []
-  },
-  {
-    'name': 'ads_krux',
-    'enabled': false,
-    'plans': []
-  },
-  {
-    'name': 'new_home',
-    'enabled': true,
-    'plans': []
-  },
+    {
+        'name': 'offline_sync',
+        'enabled': true,
+        'plans': [
+            'mid_tier',
+            'high_tier'
+        ]
+    },
+    {
+        'name': 'no_audio_ads',
+        'enabled': true,
+        'plans': [
+            'mid_tier',
+            'high_tier'
+        ]
+    },
+    {
+        'name': 'hq_audio',
+        'enabled': true,
+        'plans': [
+            'high_tier'
+        ]
+    },
+    {
+        'name': 'system_playlist_in_library',
+        'enabled': true,
+        'plans': []
+    },
+    {
+        'name': 'ads_krux',
+        'enabled': false,
+        'plans': []
+    },
+    {
+        'name': 'new_home',
+        'enabled': true,
+        'plans': []
+    },
 
-  {
-    'name': 'spotlight',
-    'enabled': false,
-    'plans': []
-  }
+    {
+        'name': 'spotlight',
+        'enabled': false,
+        'plans': []
+    }
 ]
-$notification.post('SoundCloud', 'You have activated SoundCloud GO++', 'Active GO++')
-$done({ body: JSON.stringify(obj) })
+$done({body: JSON.stringify(obj)})
 
 
