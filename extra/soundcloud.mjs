cc01f283ef3cea861b716cf8e2a18bd363c3d1bc
Nguyễn Xuân Hoàng <102905275+hoangsvn@users.noreply.github.com>
2025-08-22 05:57:02 +0700

    Update soundcloud.mjs

--- extra/soundcloud.mjs
+++ extra/soundcloud.mjs
@@ -2,29 +2,32 @@ import * as esbuild from 'esbuild'
 import fs from 'fs';
 
 const outputFile = '../module/soundcloud.module';
-const data = [
-    '﻿#!url=https://raw.githubusercontent.com/hoangsvn/hoangsvn/main/module/soundcloud.module',
-    '#!name=SoundCloud',
-    '#!desc=Active Go++',
-    '#!date=' + new Date().toLocaleString(),
-    '',
-    '[MITM]',
-    'hostname= %APPEND% api-mobile.soundcloud.com',
-    '',
-    '[Script]',
-    '',
-    'SoundCloud=type=http-response,pattern=https://api-mobile.soundcloud.com/configuration/ios,requires-body=1,script-path=https://raw.githubusercontent.com/hoangsvn/hoangsvn/main/extra/dist/soundcloud.js'
-]
-fs.writeFileSync(outputFile, data.join("\n"), "utf-8")
 
+// Build soundcloud.js trước
 esbuild.buildSync({
     entryPoints: {
         soundcloud: './src/soundcloud.js'
     },
     bundle: true,
     minify: true,
-    banner: {js: `// Build Soundcloud Start: ${new Date().toLocaleString()}`},
-    footer: {js: `// Build Soundcloud End: ${new Date().toLocaleString()}`},
     sourcemap: false,
     outdir: './dist'
-})
+});
+
+// Đọc nội dung script đã build
+const scriptCode = fs.readFileSync('./dist/soundcloud.js', 'utf-8');
+
+const data = [
+    '#!url=https://raw.githubusercontent.com/hoangsvn/hoangsvn/main/module/soundcloud.module',
+    '#!name=SoundCloud',
+    '#!desc=Active Go++ build:' + new Date().toLocaleString(),
+    '',
+    '[MITM]',
+    'hostname= %APPEND% api-mobile.soundcloud.com',
+    '',
+    '[Script]',
+    '',
+    `SoundCloud=type=http-response,pattern=https://api-mobile.soundcloud.com/configuration/ios,requires-body=1,script=${scriptCode}`
+];
+
+fs.writeFileSync(outputFile, data.join("\n"), "utf-8");
